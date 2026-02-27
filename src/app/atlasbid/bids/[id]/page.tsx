5f4598c169d0fd379bb01185ac514afcfb60441e
devri1mr <matthew@garpielgroup.com>
2026-02-28 00:43:54 +0100

    Update page.tsx

--- src/app/atlasbid/bids/[id]/page.tsx
+++ src/app/atlasbid/bids/[id]/page.tsx
@@ -1,8 +1,7 @@
-// src/app/atlasbid/bids/[id]/page.tsx
 import BidDetailClient from "./BidDetailClient";
 
 export const dynamic = "force-dynamic";
 
 export default async function Page({ params }: { params: { id: string } }) {
-  return <BidDetailClient id={params.id} />;
+  return <BidDetailClient bidId={params.id} />;
 }
