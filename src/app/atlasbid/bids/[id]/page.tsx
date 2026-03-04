8b096374112e41d5e59e0077baf77268687667d2
devri1mr <matthew@garpielgroup.com>
2026-03-04 22:57:51 +0100

    Update page.tsx

--- src/app/atlasbid/bids/[id]/page.tsx
+++ src/app/atlasbid/bids/[id]/page.tsx
@@ -1,11 +1,6 @@
 // src/app/atlasbid/bids/[id]/page.tsx
 import BidDetailClient from "./BidDetailClient";
 
-export default async function Page({
-  params,
-}: {
-  params: Promise<{ id: string }>;
-}) {
-  const { id } = await params;
-  return <BidDetailClient bidId={id} />;
+export default function Page({ params }: { params: { id: string } }) {
+  return <BidDetailClient bidId={params.id} />;
 }
