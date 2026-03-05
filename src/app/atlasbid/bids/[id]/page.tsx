bda54b219520f751ca5a37e43f9cfed72a19c2fc
devri1mr <matthew@garpielgroup.com>
2026-03-05 13:54:52 +0100

    Update page.tsx

--- src/app/atlasbid/bids/[id]/page.tsx
+++ src/app/atlasbid/bids/[id]/page.tsx
@@ -1,6 +1,11 @@
 // src/app/atlasbid/bids/[id]/page.tsx
+
 import BidDetailClient from "./BidDetailClient";
 
-export default function Page({ params }: { params: { id: string } }) {
+export default function Page({
+  params,
+}: {
+  params: { id: string };
+}) {
   return <BidDetailClient bidId={params.id} />;
 }
